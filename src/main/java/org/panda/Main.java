0542fbbc11d96287855d772086d284141efd7e4d
Thant Zin Aung <tza.personal.dev@gmail.com>
2025-01-06 22:44:37 +0630

    After successfully implemented Youtube Data API v3

--- src/main/java/org/panda/Main.java
+++ src/main/java/org/panda/Main.java
@@ -1,6 +1,7 @@
 package org.panda;
 
 import org.panda.domains.scraping.IggGameWebScraper;
+import org.panda.domains.youtube.YoutubeDataFetcher;
 import org.panda.exceptions.ChromeRelatedException;
 
 import java.io.IOException;
@@ -8,20 +9,14 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, ChromeRelatedException {
 //        System.out.println(Initializer.initialize() ? "Ready to use..." : "Failed to use");
+//        IggGameWebScraper iggGameWebScraper = new IggGameWebScraper(2);
+//        iggGameWebScraper.start();
 
-//        AutomateBrowser automateBrowser = new AutomateBrowser();
-//        List<String> gameTitleList = new ArrayList<>(List.of("Little Nightmare", "Ghost Warrior", "Red Dead Re", "Blend", "SimSons"));
-//        gameTitleList.forEach(title -> {
-//            System.out.println(title);
-//            automateBrowser.checkGameAlreadyExist(title);
-//        });
-//        automateBrowser.closeBrowser();
-
-        IggGameWebScraper iggGameWebScraper = new IggGameWebScraper(2);
-        iggGameWebScraper.start();
-
-        //testing
-//        iggGameWebScraper.getGamePlayImages(null, null);
+        try {
+            YoutubeDataFetcher.fetch();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
