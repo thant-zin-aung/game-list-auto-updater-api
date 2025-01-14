196b061cf689c903f4cc58da84a97d46c3b76738
Thant Zin Aung <tza.personal.dev@gmail.com>
2025-01-14 23:21:29 +0630

    Added new facebook handler methods

--- src/main/java/org/panda/Main.java
+++ src/main/java/org/panda/Main.java
@@ -10,10 +10,11 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, ChromeRelatedException {
 //        System.out.println(Initializer.initialize() ? "Ready to use..." : "Failed to use");
-        IggGameWebScraper iggGameWebScraper = new IggGameWebScraper(2);
-        iggGameWebScraper.start();
+//        IggGameWebScraper iggGameWebScraper = new IggGameWebScraper(2);
+//        iggGameWebScraper.start();
 
-//        FacebookHandler.post();
+//        FacebookHandler.extendPageAccessToken();
+//        FacebookHandler.post("EAAIWWKAz7kgBOZCp8mnp51hYI7FkRtCe3g4GovPe9irNFhP1TTPa39TfyLEz2Va7QzJKMXoR59XN4YIOQm8yLYZCHQhTgrzxm6SxZAuiRxHGlkNVpltbbttzTZC6ZCljEG9g4j529EJGbdyqZBqdmlyZC3SLeZCfZBrgMsKBuUZBwXQlizA0FUHGcaZAzTZAN5P3sJ0ZD");
 
     }
 }
--- src/main/java/org/panda/domains/facebook/FacebookHandler.java
+++ src/main/java/org/panda/domains/facebook/FacebookHandler.java
@@ -1,18 +1,23 @@
 package org.panda.domains.facebook;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import okhttp3.*;
 
 import java.io.File;
 import java.io.IOException;
 
 public class FacebookHandler {
-    static String endPoint = "https://graph.facebook.com/v21.0/208701392320007/photos?access_token=EAAIWWKAz7kgBO1EHcG8ZBBTV0VoxmSMTWBhBqvsKBriLBXfqedXpDUqyxCZAc5qe410jOjx43ppC4KihZB3AibCcFwfB7p2YZAYg29GG3sOKE3p6m3vmxQRfRiwnsRDaDDYFF8ZCZCbQqTwrJHDC0DT8ZBCpKZBpZCDfAgS1Xq5H7DUAihxXWq9QEL8edGYx1xfr5fsZAbTpfrpSo2Xb8ku0QE1lwZD&message=Beautiful Wallpaper 3";
-    public static void post() {
-        String endPointUrl = "https://graph.facebook.com/v21.0/208701392320007/photos";
-        String imagePath = "C:\\Users\\black\\Pictures\\My Dream Setups\\ss33.png";
-        String accessToken = "EAAIWWKAz7kgBO0JRMWxPPbdlZCmzgPJkcwtYDmztlNLsas1m5Q7wy8j9FuEvIZCoSW4uxwbZBKRxzzZCACuzNZCVLygdAUonTEoeZCsrZCagt7NBXXxw0lhZAxRZCjpSEz0FXe31ai3jZBiS6NXBIYXkCUcQKGa93UmU8LZAYzSPyQ9sGKj0ptUZCGvucuUCACgZAk7a8b1dZAGLkTMXJUDI1BevkZBSKIB";
-        String message = "Dream Setup 1";
-        System.out.println(endPoint);
+    private static final String BASE_URL = "https://graph.facebook.com/v21.0";
+    static String appScopeUserId = "2295734984145341";
+    static String appId = "587519854112328";
+    static String appSecret = "9201df7ce8f1799dab84f6712b88a550";
+    static String shortLiveAccessToken = "EAAIWWKAz7kgBOZCpgdtiZApCdcWNZCxmbNAlWQYdukt0wtrwYCOoKEHUJTmlY0urTatONHcZBc4ZBoAzA3cdBMwkXaFxdjTFPhOWlB1l15qgFZChZBoKMQyJzFn866brvCZBMcl7SDeJ5MxNTEoN8OhZCBdCMRmqukxx24Yw4aCzJ2NmEACWYrexgijTpur91SpXs9GvGF54UVlEmEQvW8BNqeHoZD";
+    static String longLivePageAccessToken = null;
+    public static void post(String pageAccessToken) {
+        String endPointUrl = BASE_URL+"/208701392320007/photos";
+        String imagePath = "C:\\Users\\black\\Pictures\\My Dream Setups\\ss34.png";
+        String message = "Dream Setup 10";
 
         // Define the OkHttpClient
         OkHttpClient client = new OkHttpClient();
@@ -26,7 +31,7 @@ public class FacebookHandler {
         // Create the text part of the request
         RequestBody textBody = new MultipartBody.Builder()
                 .setType(MultipartBody.FORM)
-                .addFormDataPart("access_token", accessToken)
+                .addFormDataPart("access_token", pageAccessToken)
                 .addFormDataPart("message", message)
                 .addFormDataPart("source", imageFile.getName(), imageBody)
                 .build();
@@ -45,6 +50,63 @@ public class FacebookHandler {
                 System.out.println("Response: "+ response.body().string());
                 System.out.println("Request failed with code: " + response.code());
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getLongLivedUserToken(String appId, String appSecret, String shortLivedToken) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        HttpUrl url = HttpUrl.parse(BASE_URL + "/oauth/access_token").newBuilder()
+                .addQueryParameter("grant_type", "fb_exchange_token")
+                .addQueryParameter("client_id", appId)
+                .addQueryParameter("client_secret", appSecret)
+                .addQueryParameter("fb_exchange_token", shortLivedToken)
+                .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response);
+            }
+            JsonObject jsonObject = JsonParser.parseString(response.body().string()).getAsJsonObject();
+            System.out.println("long live user token: "+jsonObject.get("access_token").getAsString());
+            return jsonObject.get("access_token").getAsString();
+        }
+    }
+
+    private static String getPageAccessToken(String longLivedUserToken) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        HttpUrl url = HttpUrl.parse(BASE_URL + "/"+appScopeUserId+"/accounts").newBuilder()
+                .addQueryParameter("access_token", longLivedUserToken)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response);
+            }
+
+            return response.body().string(); // JSON response with the page access token
+        }
+    }
+
+    public static void extendPageAccessToken() {
+
+        try {
+            // Step 1: Get Long-Lived User Token
+            String longLivedUserToken = getLongLivedUserToken(appId, appSecret, shortLiveAccessToken);
+            // Step 2: Get Page Access Token
+            String pageAccessTokenJson = getPageAccessToken(longLivedUserToken);
+            System.out.println("Page Access Token Response: " + pageAccessTokenJson);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
